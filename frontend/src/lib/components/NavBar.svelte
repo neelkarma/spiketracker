8b9c755d7f9e31db45995e7c6fb60cad54dd171f
Neel Sharma <nsdj.sharma@gmail.com>
2024-02-19 23:18:40 +1100

    update navbar

--- frontend/src/lib/components/NavBar.svelte
+++ frontend/src/lib/components/NavBar.svelte
@@ -1,15 +1,25 @@
-<nav class="navbar" role="navigation" aria-label="main navigation">
+<script lang="ts">
+  import { goto } from "$app/navigation";
+
+  const handleLogout = async () => {
+    await fetch("/api/auth/logout");
+    goto("/");
+  };
+</script>
+
+<nav class="navbar" aria-label="main navigation">
   <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
+    <a class="navbar-item" href="/app">
       <h1>SpikeTracker</h1>
     </a>
 
+    <!-- svelte-ignore a11y-missing-attribute -->
     <a
       role="button"
       class="navbar-burger"
       aria-label="menu"
       aria-expanded="false"
-      data-target="navbarBasicExample"
+      data-target="navbar"
     >
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
@@ -17,23 +27,17 @@
     </a>
   </div>
 
-  <div id="navbarBasicExample" class="navbar-menu">
+  <div id="navbar" class="navbar-menu">
     <div class="navbar-start">
-      <a class="navbar-item">Dashboard</a>
-
-      <a class="navbar-item">Matches</a>
-
-      <a class="navbar-item">Teams</a>
+      <a class="navbar-item" href="/app">Dashboard</a>
+      <a class="navbar-item" href="/app/matches">Matches</a>
+      <a class="navbar-item" href="/app/teams">Teams</a>
+      <a class="navbar-item" href="/app/players">Players</a>
     </div>
 
     <div class="navbar-end">
       <div class="navbar-item">
-        <div class="buttons">
-          <a class="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button is-light"> Log in </a>
-        </div>
+        <button class="button is-light" on:click={handleLogout}>Log Out</button>
       </div>
     </div>
   </div>
