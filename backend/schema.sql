625fe75a6ff9a0fc3c68160331d7cbfbf8e55610
Neel Sharma <nsdj.sharma@gmail.com>
2024-02-19 23:53:23 +1100

    update schema.sql with triggers and some inital data

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,9 +1,18 @@
+-- Schema
 CREATE TABLE IF NOT EXISTS players (
   id INTEGER PRIMARY KEY,
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
   grad_year INTEGER NOT NULL,
-  score INTEGER NOT NULL DEFAULT 0
+  
+  avg_ppg REAL NOT NULL DEFAULT 0,
+  kill_rate REAL NOT NULL DEFAULT 0,
+  pass_efficiency REAL NOT NULL DEFAULT 0,
+  total_points INTEGER NOT NULL DEFAULT 0,
+  total_atk_points INTEGER NOT NULL DEFAULT 0,
+  total_def_points INTEGER NOT NULL DEFAULT 0,
+  total_blk_points INTEGER NOT NULL DEFAULT 0,
+  total_srv_points INTEGER NOT NULL DEFAULT 0,
 );
 
 CREATE TABLE IF NOT EXISTS teams (
@@ -11,14 +20,28 @@ CREATE TABLE IF NOT EXISTS teams (
   name TEXT NOT NULL,
   year INTEGER NOT NULL,
   archived BOOLEAN NOT NULL DEFAULT 0
+
+  wins INTEGER NOT NULL DEFAULT 0,
+  losses INTEGER NOT NULL DEFAULT 0,
+  set_ratio REAL NOT NULL DEFAULT 0,
+  kill_rate REAL NOT NULL DEFAULT 0,
+  pass_efficiency REAL NOT NULL DEFAULT 0,
 );
 
 CREATE TABLE IF NOT EXISTS matches (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   team_id INTEGER NOT NULL REFERENCES teams(id),
+  opponent_name TEXT NOT NULL,
   approved BOOLEAN NOT NULL DEFAULT 0,
   time TEXT NOT NULL,
-  location TEXT NOT NULL
+  location TEXT NOT NULL,
+
+  set_one_our_score INTEGER NOT NULL DEFAULT 0,
+  set_one_opp_score INTEGER NOT NULL DEFAULT 0,
+  set_two_our_score INTEGER NOT NULL DEFAULT 0,
+  set_two_opp_score INTEGER NOT NULL DEFAULT 0,
+  set_three_our_score INTEGER NOT NULL DEFAULT 0,
+  set_three_opp_score INTEGER NOT NULL DEFAULT 0,
 );
 
 CREATE TABLE IF NOT EXISTS stats (
@@ -39,3 +62,45 @@ CREATE TABLE IF NOT EXISTS team_players (
   player_id INTEGER NOT NULL REFERENCES players(id),
   PRIMARY KEY (team_id, player_id)
 );
+
+-- Triggers
+CREATE TRIGGER IF NOT EXISTS update_player_on_insert
+AFTER INSERT ON stats
+BEGIN
+  UPDATE players SET
+  avg_ppg =
+    (SELECT COUNT(*) FROM stats WHERE player_id = NEW.player_id)
+    / (SELECT COUNT(*) FROM matches WHERE
+    team_id IN (SELECT team_id FROM player_teams WHERE player_id = NEW.player_id)),
+  kill_rate = 0, -- ask arnav about how to calculate kr
+  pass_efficiency = 0, -- ask arnav about how to calculate pef
+  total_points = total_points + 1,
+  total_atk_points = total_atk_points + (NEW.action = 'attack'),
+  total_def_points = total_def_points + (NEW.action = 'block'),
+  total_blk_points = total_blk_points + (NEW.action = 'block'),
+  total_srv_points = total_srv_points + (NEW.action = 'serve')
+  WHERE id = NEW.player_id;
+END;
+
+-- TODO: Create triggers for:
+-- - update_player_on_update
+-- - update_player_on_delete
+-- - update_team_on_update
+-- - update_team_on_delete
+-- - update_team_on_insert
+
+-- Data
+INSERT INTO players (id, first_name, last_name, grad_year) VALUES
+(440805299, 'Neel', 'Sharma', 2024),
+(447507684, 'Arnav', 'Gupta', 2024),
+(442097500, 'Andrew', 'Wang', 2024),
+(442031151, 'Nathanael', 'Thie', 2024);
+
+INSERT INTO teams (name, year) VALUES
+('Test', 2024);
+
+INSERT INTO team_players (team_id, player_id) VALUES
+(1, 440805299),
+(1, 447507684),
+(1, 442097500),
+(1, 442031151);
