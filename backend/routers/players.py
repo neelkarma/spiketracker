196256519881ef3023688411042a2bebcc31ac1a
leqir <442031151@student.sbhs.nsw.edu.au>
2024-02-19 11:06:58 +1100

    Add /players delete endpoint

--- backend/routers/players.py
+++ backend/routers/players.py
@@ -9,6 +9,9 @@ players = Blueprint("players", __name__)
 @players.get("/me")
 def me():
     session = get_session()
+    if session is None:
+        return "Unauthorized", 401
+    
     if session["admin"]:
         return "You are an admin", 400
 
@@ -16,3 +19,20 @@ def me():
     con.execute(
         "SELECT first_name, last_name FROM players WHERE id = ?", (session["id"],)
     )
+
+@players.delete("/<id>")
+def delete_player(id: int):
+    session = get_session()
+    if session is None:
+        return "Unauthorized", 401
+    
+    if not session["admin"]:
+        return "You a student", 401
+    
+    con = get_db()
+    con.execute(
+        "DELETE FROM players WHERE id = ?", (id,)
+    )
+
+    return "Success", 200
+
