d4467870b60da931825b13372b71f05514c6b12e
leqir <442031151@student.sbhs.nsw.edu.au>
2024-02-22 11:46:10 +1100

    fix frontend crash

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,20 +1,12 @@
 import { building } from "$app/environment";
 import { redirect, type Handle } from "@sveltejs/kit";
-import { Agent } from "undici";
-
-if (!building) {
-  //@ts-expect-error
-  globalThis[Symbol.for("undici.globalDispatcher.1")] = new Agent({
-    headersTimeout: 10 * 60e3,
-    bodyTimeout: 10 * 60e3,
-    keepAliveTimeout: 10 * 60e3,
-  });
-}
 
 export const handle: Handle = async ({ event, resolve }) => {
+  const { fetch } = event;
+
   if (event.url.pathname.startsWith("/api")) return resolve(event);
 
-  const res = await event.fetch("/api/auth/status");
+  const res = await fetch("/api/auth/status");
   const data = await res.json();
 
   if (data.authorized && !event.url.pathname.startsWith("/app"))
