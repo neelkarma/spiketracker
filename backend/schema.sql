cc6b34d3f30382c77bb9104d9b425838369032e3
Neel Sharma <nsdj.sharma@gmail.com>
2024-02-26 10:25:35 +1100

    update db schema

--- backend/schema.sql
+++ backend/schema.sql
@@ -18,23 +18,23 @@ CREATE TABLE IF NOT EXISTS teams (
 
 CREATE TABLE IF NOT EXISTS matches (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  team_id INTEGER NOT NULL REFERENCES teams(id),
+  team_id INTEGER NOT NULL REFERENCES teams (id),
   opponent_name TEXT NOT NULL,
   approved BOOLEAN NOT NULL DEFAULT 0,
   time TEXT NOT NULL,
   location TEXT NOT NULL,
-  -- set_one_our_score INTEGER NOT NULL DEFAULT 0,
+  set_one_our_score INTEGER NOT NULL DEFAULT 0,
   set_one_opp_score INTEGER NOT NULL DEFAULT 0,
-  -- set_two_our_score INTEGER NOT NULL DEFAULT 0,
+  set_two_our_score INTEGER NOT NULL DEFAULT 0,
   set_two_opp_score INTEGER NOT NULL DEFAULT 0,
-  -- set_three_our_score INTEGER NOT NULL DEFAULT 0,
+  set_three_our_score INTEGER NOT NULL DEFAULT 0,
   set_three_opp_score INTEGER NOT NULL DEFAULT 0,
 );
 
 CREATE TABLE IF NOT EXISTS stats (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  player_id INTEGER NOT NULL REFERENCES players(id),
-  match_id INTEGER NOT NULL REFERENCES matches(id),
+  player_id INTEGER NOT NULL REFERENCES players (id),
+  match_id INTEGER NOT NULL REFERENCES matches (id),
   action TEXT CHECK (
     action in (
       'attack',
@@ -74,163 +74,111 @@ CREATE TABLE IF NOT EXISTS stats (
 );
 
 CREATE TABLE IF NOT EXISTS team_players (
-  team_id INTEGER NOT NULL REFERENCES teams(id),
-  player_id INTEGER NOT NULL REFERENCES players(id),
+  team_id INTEGER NOT NULL REFERENCES teams (id),
+  player_id INTEGER NOT NULL REFERENCES players (id),
   PRIMARY KEY (team_id, player_id)
 );
 
 -- Views
+CREATE VIEW IF NOT EXISTS points AS
+SELECT *
+FROM stats
+WHERE action IN ('attack', 'block', 'serve');
+
+CREATE VIEW IF NOT EXISTS successful_points AS
+SELECT *
+FROM points
+WHERE rating = 3;
+
+CREATE VIEW IF NOT EXISTS passes AS
+SELECT *
+FROM stats
+WHERE action in ('serve_receive', 'freeball_receive', 'set');
+
+CREATE VIEW IF NOT EXISTS successful_passes AS
+SELECT *
+FROM passes
+WHERE rating = 3;
+
 CREATE VIEW IF NOT EXISTS player_stats AS
-SELECT
-  id,
+SELECT id,
   (
-    SELECT
-      COUNT(*)
-    FROM
-      stats
-    WHERE
-      stats.player_id = players.id
-      AND stats.action IN ('attack', 'block')
-      AND stats.rating = 3 -- ask arnav what qualifies as a point
+    SELECT COUNT(*)
+    FROM successful_points
+    WHERE successful_points.player_id = players.id
   ) / (
-    SELECT
-      COUNT(*)
-    FROM
-      matches
-    WHERE
-      matches.team_id IN (
-        SELECT
-          team_id
-        FROM
-          team_players
-        WHERE
-          player_id = players.id
-      ) = players.id
+    SELECT COUNT(*)
+    FROM matches
+    WHERE matches.team_id IN (
+        SELECT team_id
+        FROM team_players
+        WHERE player_id = players.id
+      ) = players.id -- write this as a join?
   ) AS avg_ppg,
   (
-    SELECT
-      COUNT(*)
-    FROM
-      stats
-    WHERE
-      stats.player_id = players.id
-      AND stats.action = 'attack'
-      AND stats.rating = 3
+    SELECT COUNT(*)
+    FROM successful_points
+    WHERE stats.player_id = players.id
   ) / (
-    SELECT
-      COUNT(*)
-    FROM
-      stats
-    WHERE
-      stats.player_id = players.player_id,
-      AND stats.action = 'attack'
+    SELECT COUNT(*)
+    FROM points
+    WHERE stats.player_id = players.player_id
   ) AS kill_rate,
   (
-    SELECT
-      COUNT(*)
-    FROM
-      stats
-    WHERE
-      stats.player_id = players.id
-      AND stats.action = 'set' -- ask arnav if there are other actions which constitute a "pass"
-      AND stats.rating = 3
+    SELECT COUNT(*)
+    FROM successful_passes
+    WHERE stats.player_id = players.id
   ) / (
-    SELECT
-      COUNT(*)
-    FROM
-      stats
-    WHERE
-      stats.player_id = players.player_id
-      AND stats.action = 'set'
+    SELECT COUNT(*)
+    FROM passes
+    WHERE stats.player_id = players.player_id
   ) AS pass_efficiency,
   (
-    SELECT
-      COUNT(*)
-    FROM
-      stats
-    WHERE
-      stats.player_id = players.id
-      AND stats.action IN ('attack', 'block') -- again, what constitutes a "point"?
-  ) AS total_points,
-  (
-    SELECT
-      COUNT(*)
-    FROM
-      stats
-    WHERE
-      stats.player_id = players.id
-      AND stats.action IN ('attack')
-  ) AS total_atk_points,
-  (
-    SELECT
-      COUNT(*)
-    FROM
-      stats
-    WHERE
-      stats.player_id = players.id
-      AND stats.action IN ('block')
-  ) AS total_blk_points,
-  -- ?
-  (
-    SELECT
-      COUNT(*)
-    FROM
-      stats
-    WHERE
-      stats.player_id = players.id
-      AND stats.action IN ('serve')
-  ) AS total_srv_points,
-  -- ?
-  (
-    SELECT
-      COUNT(*)
-    FROM
-      stats
-    WHERE
-      stats.player_id = players.id
-      AND stats.action IN ('serve_receive', 'freeball_recieve', 'set')
-  ) AS total_def_points -- ?
-FROM
-  players;
+    SELECT COUNT(*)
+    FROM successful_points
+    WHERE stats.player_id = players.id
+  ) AS total_points
+FROM players;
+
+CREATE VIEW IF NOT EXISTS match_stats AS
+SELECT id,
+  -- total our sets
+  -- total opp sets
+  -- total our points for each set
+FROM matches;
 
 CREATE VIEW IF NOT EXISTS team_stats AS
-SELECT
-  id,
+SELECT id,
+  (
+    SELECT COUNT(*)
+    FROM matches
+    WHERE matches.team_id = teams.id
+  ) AS total_matches,
+  (
+    SELECT COUNT(*)
+    FROM matches
+    WHERE matches.team_id = teams.id
+      AND matches.approved = 1
+  ) AS total_approved_matches,
   -- wins INTEGER NOT NULL DEFAULT 0,
   -- losses INTEGER NOT NULL DEFAULT 0,
   -- set_ratio REAL NOT NULL DEFAULT 0,
   -- kill_rate REAL NOT NULL DEFAULT 0,
-  () -- pass_efficiency REAL NOT NULL DEFAULT 0,
-FROM
-  teams;
-
-CREATE VIEW IF NOT EXISTS match_stats AS
-SELECT
-  id,
-  -- total our sets
-  -- total opp sets
-  -- total our points for each set
-FROM
-  matches;
+  -- pass_efficiency REAL NOT NULL DEFAULT 0,
+FROM teams;
 
 -- Data
-INSERT INTO
-  players (id, first_name, last_name, grad_year)
-VALUES
-  (440805299, 'Neel', 'Sharma', 2024),
+INSERT INTO players (id, first_name, last_name, grad_year)
+VALUES (440805299, 'Neel', 'Sharma', 2024),
   (447507684, 'Arnav', 'Gupta', 2024),
   (442097500, 'Andrew', 'Wang', 2024),
   (442031151, 'Nathanael', 'Thie', 2024);
 
-INSERT INTO
-  teams (name, year)
-VALUES
-  ('Test', 2024);
+INSERT INTO teams (name, year)
+VALUES ('Test', 2024);
 
-INSERT INTO
-  team_players (team_id, player_id)
-VALUES
-  (1, 440805299),
+INSERT INTO team_players (team_id, player_id)
+VALUES (1, 440805299),
   (1, 447507684),
   (1, 442097500),
   (1, 442031151);
