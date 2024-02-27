38939d1b7b90703f66fe423a286861435ee999cb
Neel Sharma <nsdj.sharma@gmail.com>
2024-02-28 10:00:47 +1100

    implement basic modal component

---
+++ frontend/src/lib/components/Modal.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let isOpen = false;
+</script>
+
+<div class="modal" class:is-active={isOpen}>
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <div class="modal-background" on:click={() => (isOpen = false)}></div>
+  <div class="modal-content"><slot /></div>
+  <button
+    class="modal-close is-large"
+    aria-label="close"
+    on:click={() => (isOpen = false)}
+  ></button>
+</div>
