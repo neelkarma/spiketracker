9eab282bd9f8d99e1a474ae5b9c8c71c1f6e137d
Neel Sharma <nsdj.sharma@gmail.com>
2024-02-28 21:53:24 +1100

    commit war crimes making the navbar responsive

--- frontend/src/lib/components/NavBar.svelte
+++ frontend/src/lib/components/NavBar.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
 
+  let isOpen = false;
+
   const handleLogout = async () => {
     await fetch("/api/auth/logout");
     goto("/");
@@ -13,6 +15,8 @@
       <h1 class="has-text-weight-bold">SpikeTracker</h1>
     </a>
 
+    <!-- svelte-ignore a11y-interactive-supports-focus -->
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
     <!-- svelte-ignore a11y-missing-attribute -->
     <a
       role="button"
@@ -20,6 +24,7 @@
       aria-label="menu"
       aria-expanded="false"
       data-target="navbar"
+      on:click={() => (isOpen = !isOpen)}
     >
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
@@ -27,7 +32,7 @@
     </a>
   </div>
 
-  <div id="navbar" class="navbar-menu">
+  <div id="navbar" class="navbar-menu" class:is-active={isOpen}>
     <div class="navbar-start">
       <a class="navbar-item" href="/app">Dashboard</a>
       <a class="navbar-item" href="/app/matches">Matches</a>
@@ -37,25 +42,50 @@
 
     <div class="navbar-end">
       <div class="navbar-item">
-        <a href="/app/settings" class="has-text-white" title="Settings">
+        <a
+          href="/app/settings"
+          title="Settings"
+          class="has-text-white is-hidden-touch"
+        >
           <i class="fa-solid fa-gear"></i>
         </a>
-      </div>
-      <div class="navbar-item">
-        <a href="/app/help" class="has-text-white" title="Help">
-          <i class="fa-solid fa-question-circle"></i>
+        <a href="/app/settings" class="is-hidden-desktop">
+          <i class="fa-solid fa-gear"></i>
+          <span>Settings</span>
         </a>
       </div>
       <div class="navbar-item">
-        <!-- svelte-ignore a11y-invalid-attribute -->
+        <a href="/app/help" title="Help" class="has-text-white is-hidden-touch">
+          <i class="fa-solid fa-question-circle"></i>
+        </a>
+        <a href="/app/help" class="is-hidden-desktop">
+          <i class="fa-solid fa-question-circle"></i>
+          <span>Help</span>
+        </a>
+      </div>
+      <div class="navbar-item">
+        <!-- svelte-ignore a11y-interactive-supports-focus -->
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-missing-attribute -->
         <a
           on:click={handleLogout}
-          class="has-text-white"
+          class="has-text-white is-hidden-touch"
           role="button"
-          href="#"
           title="Log Out"
         >
           <i class="fa-solid fa-sign-out-alt"></i>
+        </a>
+        <!-- svelte-ignore a11y-interactive-supports-focus -->
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <a
+          on:click={handleLogout}
+          class="is-hidden-desktop"
+          role="button"
+          title="Log Out"
+        >
+          <i class="fa-solid fa-sign-out-alt"></i>
+          <span>Log Out</span>
         </a>
       </div>
     </div>
