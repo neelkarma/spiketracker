ebe722ab8dca7a6b628888e09418d0590d0e33cb
leqir <442031151@student.sbhs.nsw.edu.au>
2024-02-29 12:30:29 +1100

    make quick start help page

---
+++ frontend/src/routes/app/help/+layout.svelte
@@ -0,0 +1,40 @@
+<section class="section">
+  <div class="container">
+    <h1 class="title">Help</h1>
+
+    <div class="columns">
+      <div class="column is-one-quarter">
+        <aside class="menu">
+          <p class="menu-label">Users</p>
+          <ul class="menu-list">
+            <li><a href="">Quick Help</a></li>
+            <li><a>Dashboard</a></li>
+            <li><a>Search</a></li>
+            <li><a>Sort</a></li>
+            <li><a>Matches</a></li>
+            <li><a>Players</a></li>
+            <li><a>Teams</a></li>
+          </ul>
+          <p class="menu-label">Administration</p>
+          <ul class="menu-list">
+            <li><a>Reviewing Matches</a></li>
+            <li>
+              <a class="is-active">Team Settings</a>
+              <ul>
+                <li><a>Members</a></li>
+                <li><a>Plugins</a></li>
+                <li><a>Add a member</a></li>
+              </ul>
+            </li>
+            <li><a>Scoring</a></li>
+            <li><a>Andrew Wang</a></li>
+            <li><a>Authentication</a></li>
+          </ul>
+        </aside>
+      </div>
+      <div class="column">
+        <slot />
+      </div>
+    </div>
+  </div>
+</section>
---
+++ frontend/src/routes/app/help/quick-start/+page.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import navbarImage from "../../../../assets/navbar-image.jpg";
+</script>
+
+<div class="content">
+  <h1>Quick Start</h1>
+  <p>
+    Welcome to SpikeTracker! SpikeTracker is your all-in-one solution for
+    efficiently managing and tracking statistics for your high school's
+    volleyball program. Whether you're a coach or a player, SpikeTracker
+    provides the tools you need to stay organized and informed throughout the
+    volleyball season.
+  </p>
+  <h4>Navigation</h4>
+
+  <figure class="navbar-image">
+    <img src={navbarImage} alt="Navbar image" />
+  </figure>
+  <ol>
+    <li>
+      <strong>Dashboard - </strong>This contains information about upcomimg
+      matches, and your personal and team stats. For admins, the Dashboard
+      displays the matches to approve and other administrative actions.
+    </li>
+    <li>
+      <strong>Matches - </strong>The Matches pages displays all matches
+      involving High. All information and statistics for each set and each
+      player are shown for each match.
+    </li>
+    <li>
+      <strong>Players - </strong>The Players pages displays all volleyball
+      players at High, and their individual stats, including match history,
+      average PPG, kill rate, passing efficiency and total points across the
+      season.
+    </li>
+    <li>
+      <strong>Teams - </strong>The Teams page show all teams at High, and their
+      stats such as W/L, total points, as well as upcoming and past matches.
+    </li>
+    <li>
+      <strong>Search - </strong>You can search for any specific match, team or
+      player here.
+    </li>
+    <li><strong>Settings - </strong>Takes you to the settings page.</li>
+    <li><strong>Help - </strong>Takes you to this page..</li>
+    <li><strong>Logout - </strong>Logs you out of your account.</li>
+  </ol>
+</div>
