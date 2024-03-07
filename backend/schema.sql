2cbcfd4b27b9aba87a34ae4a6f8b44f94adc1e60
arnav-gupta07 <447507684@student.sbhs.nsw.edu.au>
2024-03-08 10:22:10 +1100

    Started making error view

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,6 +1,5 @@
 -- Pragmas
 PRAGMA foreign_keys = ON;
-
 -- Tables
 CREATE TABLE IF NOT EXISTS players (
   id INTEGER PRIMARY KEY,
@@ -9,14 +8,12 @@ CREATE TABLE IF NOT EXISTS players (
   grad_year INTEGER NOT NULL,
   positions TEXT NOT NULL -- string[]
 );
-
 CREATE TABLE IF NOT EXISTS teams (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL,
   year INTEGER NOT NULL,
   archived BOOLEAN NOT NULL DEFAULT 0
 );
-
 CREATE TABLE IF NOT EXISTS matches (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   team_id INTEGER NOT NULL REFERENCES teams (id),
@@ -26,7 +23,6 @@ CREATE TABLE IF NOT EXISTS matches (
   location TEXT NOT NULL,
   points TEXT NOT NULL -- { our: number; opp: number; }[]
 );
-
 CREATE TABLE IF NOT EXISTS stats (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   player_id INTEGER NOT NULL REFERENCES players (id),
@@ -68,34 +64,28 @@ CREATE TABLE IF NOT EXISTS stats (
     AND y_pos_out <= 18
   ) NOT NULL
 );
-
 CREATE TABLE IF NOT EXISTS team_players (
   team_id INTEGER NOT NULL REFERENCES teams (id),
   player_id INTEGER NOT NULL REFERENCES players (id),
   PRIMARY KEY (team_id, player_id)
 );
-
 -- Views
 CREATE VIEW IF NOT EXISTS points AS
 SELECT *
 FROM stats
 WHERE action IN ('attack', 'block', 'serve');
-
 CREATE VIEW IF NOT EXISTS successful_points AS
 SELECT *
 FROM points
 WHERE rating = 3;
-
 CREATE VIEW IF NOT EXISTS passes AS
 SELECT *
 FROM stats
 WHERE action in ('serve_receive', 'freeball_receive', 'set');
-
 CREATE VIEW IF NOT EXISTS successful_passes AS
 SELECT *
 FROM passes
 WHERE rating = 3;
-
 CREATE VIEW IF NOT EXISTS player_stats AS
 SELECT id,
   (
@@ -135,14 +125,12 @@ SELECT id,
     WHERE stats.player_id = players.id
   ) AS total_points
 FROM players;
-
 CREATE VIEW IF NOT EXISTS match_stats AS
 SELECT id,
   -- total our sets
   -- total opp sets
   -- total our points for each set
 FROM matches;
-
 CREATE VIEW IF NOT EXISTS team_stats AS
 SELECT id,
   (
@@ -162,7 +150,21 @@ SELECT id,
   -- kill_rate REAL NOT NULL DEFAULT 0,
   -- pass_efficiency REAL NOT NULL DEFAULT 0,
 FROM teams;
-
+-- not complete
+CREATE VIEW IF NOT EXISTS error_stats AS
+SELECT id,
+  (
+    SELECT *
+    FROM stats
+    WHERE stats.rating = 0
+  ) AS total_matches,
+  (
+    SELECT COUNT(*)
+    FROM matches
+    WHERE matches.team_id = teams.id
+      AND matches.approved = 1
+  ) AS total_approved_matches,
+  FROM teams;
 -- Data
 INSERT
   OR IGNORE INTO players (id, first_name, last_name, grad_year)
@@ -170,11 +172,9 @@ VALUES (440805299, 'Neel', 'Sharma', 2024),
   (447507684, 'Arnav', 'Gupta', 2024),
   (442097500, 'Andrew', 'Wang', 2024),
   (442031151, 'Nathanael', 'Thie', 2024);
-
 INSERT
   OR IGNORE INTO teams (name, year)
 VALUES ('Test', 2024);
-
 INSERT
   OR IGNORE INTO team_players (team_id, player_id)
 VALUES (1, 440805299),
