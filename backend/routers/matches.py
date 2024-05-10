12a41b5c39d546d25d48bcf034bcdaf79d4d6f61
Kramig <442097500@student.sbhs.nsw.edu.au>
2024-05-10 12:32:48 +1000

    edited teams, matches, players .py, and fixed border issue on mobile interface for login page

--- backend/routers/matches.py
+++ backend/routers/matches.py
@@ -1,9 +1,6 @@
-import json
-
-import jsonify
-from flask import Blueprint
-
-from backend.db import get_db
+from db import get_db
+from flask import Blueprint, jsonify
+from session import get_session
 
 matches = Blueprint("/matches", __name__)
 
@@ -44,3 +41,24 @@ def get_match_data(id: int):
             "scoring": data["scoring"]
         }
     )
+
+    @matches.post("/<id>")
+    def get_attribute_to_edit(value: str):
+        pass
+    def get_new_value(value: int):
+        pass
+
+    def update_matches(id: int):
+        session = get_session()
+        if session is None:
+            return "Unauthorized", 401
+        
+        if not session['admin']:
+            return "You are a student", 401
+    
+        attribute = get_attribute_to_edit()
+        new_value = get_new_value()
+        con = get_db()
+        con.execute("UPDATE players SET ? = ? WHERE id = ?", (attribute, new_value, id)) 
+        
+        return "Success", 200
