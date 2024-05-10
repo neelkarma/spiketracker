a183c35cb63aef0bfee4a5ea1ce622a05fd79154
Neel Sharma <nsdj.sharma@gmail.com>
2024-05-10 12:11:45 +1000

    minor additions

--- frontend/src/routes/app/scoring/[id]/+page.svelte
+++ frontend/src/routes/app/scoring/[id]/+page.svelte
@@ -4,5 +4,10 @@
   export let data: TeamInfo;
 </script>
 
+<div class="columns">
+  <div class="column is-narrow"></div>
+  <div class="column"></div>
+  <div class="column is-narrow"></div>
+</div>
 <div>aaaaaaaa</div>
 <pre>{JSON.stringify(data, undefined, 2)}</pre>
