ced1fbdde67beb04287c4a267075368168c65cce
Neel Sharma <nsdj.sharma@gmail.com>
2024-07-09 17:40:29 +1000

    add some typedefs to the auth layout load script

--- frontend/src/routes/app/+layout.ts
+++ frontend/src/routes/app/+layout.ts
@@ -1,5 +1,14 @@
 import type { LayoutLoad } from "./$types";
 
-export const load = (({ fetch }) =>
-  fetch("/api/auth/status").then((res) => res.json())
-) satisfies LayoutLoad;
+type AuthRepsonse =
+  | {
+      authorized: false;
+    }
+  | {
+      authorized: true;
+      admin: true;
+    }
+  | { authorized: true; admin: false; id: number };
+
+export const load = (({ fetch }): Promise<AuthRepsonse> =>
+  fetch("/api/auth/status").then((res) => res.json())) satisfies LayoutLoad;
