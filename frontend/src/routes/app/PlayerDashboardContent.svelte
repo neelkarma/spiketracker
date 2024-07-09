2bf417b04eadd2fc5b3559d92e4e6e121622ca56
Neel Sharma <nsdj.sharma@gmail.com>
2024-07-09 23:42:14 +1000

    use heatmap component in dashboard

--- frontend/src/routes/app/PlayerDashboardContent.svelte
+++ frontend/src/routes/app/PlayerDashboardContent.svelte
@@ -2,38 +2,32 @@
   import { page } from "$app/stores";
 
   import Stat from "$lib/components/Stat.svelte";
+  import StatHeatmap from "$lib/components/StatHeatmap.svelte";
   import TeamCard from "$lib/components/TeamCard.svelte";
 </script>
 
 <div class="block">
   <div class="title">Your Stats</div>
-  <div class="columns">
-    <div class="column">
-      <div class="box">
-        <div class="fixed-grid">
-          <div class="grid">
-            <div class="cell">
-              <Stat label="Kill Rate" value={$page.data.kr.toFixed(3)} />
-            </div>
-            <div class="cell">
-              <Stat
-                label="Passing Efficiency"
-                value={$page.data.pef.toFixed(3)}
-              />
-            </div>
-            <div class="cell">
-              <Stat label="Total Points" value={$page.data.totalPoints} />
-            </div>
-            <div class="cell">
-              <Stat label="Total Matches" value={$page.data.matchIds.length} />
-            </div>
-          </div>
+  <div class="box" style="height: 100%;">
+    <div class="fixed-grid has-4-cols has-2-cols-mobile">
+      <div class="grid">
+        <div class="cell">
+          <Stat label="Kill Rate" value={$page.data.kr.toFixed(3)} />
+        </div>
+        <div class="cell">
+          <Stat label="Passing Efficiency" value={$page.data.pef.toFixed(3)} />
+        </div>
+        <div class="cell">
+          <Stat label="Total Points" value={$page.data.totalPoints} />
+        </div>
+        <div class="cell">
+          <Stat label="Total Matches" value={$page.data.matchIds.length} />
         </div>
       </div>
     </div>
-    <div class="column">
-      <div class="box">(todo: insert heatmap here)</div>
-    </div>
+  </div>
+  <div class="box">
+    <StatHeatmap type="player" id={$page.data.id} width={280} />
   </div>
 </div>
 
@@ -45,7 +39,14 @@
       Your Teams
     {/if}
   </div>
-  {#each $page.data.teams as team}
-    <TeamCard data={team} />
-  {/each}
+  {#if $page.data.teams.length === 0}
+    <div class="block">
+      You are currently not a member of any team. Please ask your coach to add
+      you to one.
+    </div>
+  {:else}
+    {#each $page.data.teams as team}
+      <TeamCard data={team} />
+    {/each}
+  {/if}
 </div>
