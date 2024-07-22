2c64dbccf82bf2eb2bf5376139de313b2866b3e4
Neel Sharma <nsdj.sharma@gmail.com>
2024-07-22 12:39:58 +1000

    fix help prerendering failing
    according to sveltekit docs, the routes should be automatically
    recognized from the sidebar component, but that's not working so now we
    just manually tell sveltekit the pages to prerender using glob imports

--- frontend/src/routes/app/help/[slug]/+page.server.ts
+++ frontend/src/routes/app/help/[slug]/+page.server.ts
@@ -1,8 +1,18 @@
 import { error } from "@sveltejs/kit";
 import { marked } from "marked";
-import type { PageServerLoad } from "./$types";
+import type { EntryGenerator, PageServerLoad } from "./$types";
 
 export const prerender = true;
+
+export const entries: EntryGenerator = async () => {
+  const allPages = import.meta.glob("../*.md", { query: "?raw" });
+
+  return Object.keys(allPages).map((path) => ({
+    // strip the `../` and the `.md` from the path string using raw indexing
+    // since import.meta.glob preserves those
+    slug: path.substring(3, path.length - 3),
+  }));
+};
 
 export const load = (async ({ params }) => {
   try {
