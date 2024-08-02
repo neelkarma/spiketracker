4a5ae3fda493c16c24ec320ff4288e18c37f565e
Neel Sharma <nsdj.sharma@gmail.com>
2024-08-02 14:03:49 +1000

    clarify button destination in 404

--- src/routes/[...404]/+page.svelte
+++ src/routes/[...404]/+page.svelte
@@ -2,6 +2,6 @@
   <div class="container">
     <h1 class="title">Error 404</h1>
     <h2 class="subtitle">The page you have requested does not exist.</h2>
-    <a href="/" class="button is-primary">Go Home</a>
+    <a href="/" class="button is-primary">Go Back</a>
   </div>
 </section>
