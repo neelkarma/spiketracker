b9d6790b6f2b4f5e2fb8935fb6880ed8cac8581f
Neel Sharma <nsdj.sharma@gmail.com>
2024-08-02 14:03:24 +1000

    add 404 page

---
+++ src/routes/[...404]/+page.svelte
@@ -0,0 +1,7 @@
+<section class="section">
+  <div class="container">
+    <h1 class="title">Error 404</h1>
+    <h2 class="subtitle">The page you have requested does not exist.</h2>
+    <a href="/" class="button is-primary">Go Home</a>
+  </div>
+</section>
