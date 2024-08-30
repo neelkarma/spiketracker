c095f0a756edba1e0119f914452e53ecc5672726
Neel Sharma <nsdj.sharma@gmail.com>
2024-08-30 19:41:35 +1000

    do 404 pages properly
    this is just silly... the reason the lockdown was registering a success
    was because the /api/matches/lockdown endpoint was returning a 200, even
    though it didn't exist... because of the [...404] matcher at the top
    level of the project.
    
    i have since found that i've been doing sveltekit 404 pages completely
    wrong this entire time, and it's been corrected now. as a minor
    consequence, the error page looks slightly different, but we can live
    with that (hopefully)

---
+++ src/routes/+error.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+</script>
+
+<section class="section">
+  <div class="container content">
+    {#if $page.status === 404}
+      <h1>Error 404</h1>
+      <h2>The page you have requested does not exist.</h2>
+      <a href="/" class="button is-primary">Back to Safety</a>
+    {:else}
+      <h1>Whoops!</h1>
+      <h2>Looks like something went wrong.</h2>
+      {#if $page.data.admin}
+        <p>Please contact the developers, and send the error details below.</p>
+      {:else}
+        <p>
+          Most likely, you were not allowed access to the page you were going
+          to.
+        </p>
+        <p>If not, please contact your coach for support.</p>
+      {/if}
+      <details class="mb-2">
+        <summary>Click to expand error details.</summary>
+        <div>
+          <p>Error Code: {$page.status}</p>
+          <p>Error Message: <code>{$page.error?.message}</code></p>
+        </div>
+      </details>
+      <a href="/app" class="button is-primary">Back to Safety</a>
+    {/if}
+  </div>
+</section>
