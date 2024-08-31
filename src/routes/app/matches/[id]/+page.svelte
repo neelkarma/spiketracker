80247840f7fa10b4cfc5d432ab9d46ff992f705a
Neel Sharma <nsdj.sharma@gmail.com>
2024-08-31 13:18:57 +1000

    improve print layout for match page
    this used to be terrible. now it's less terrible.
    
    we can't adapt the existing ui layout to print, so we instead create an
    entirely separate section for print layout and hide it until printing.

--- src/routes/app/matches/[id]/+page.svelte
+++ src/routes/app/matches/[id]/+page.svelte
@@ -9,70 +9,100 @@
   $: setsWon = calculateSetsWon(data.match.points);
 </script>
 
-<!-- header with general match details -->
-<div class="hero is-primary has-background-primary-dark">
-  <div class="hero-body has-text-centered">
-    <div class="container">
-      <div class="level-left">
-        <div>
+<!-- header with general match details (not included in print layout since it is not print-friendly) -->
+<div class="hero is-primary has-background-primary-dark no-print">
+  <div class="hero-body">
+    <div class="level-left">
+      <div>
+        <a
+          href="/app/matches"
+          class="has-text-link-light is-underlined no-print"
+        >
+          <span>
+            <i class="fas fa-arrow-left"></i>
+            Back to All Matches
+          </span></a
+        >
+      </div>
+    </div>
+    <h2 class="title has-text-white">
+      {new Date(data.match.time).toDateString()}
+    </h2>
+    <h3 class="subtitle has-text-white">{data.match.location}</h3>
+    <div class="columns is-centered is-vcentered no-print">
+      <div class="column is-5">
+        <h2 class="has-text-right title">
           <a
-            href="/app/matches"
-            class="has-text-link-light is-underlined no-print"
+            href="/app/teams/{data.match.teamId}"
+            class="has-text-white is-underlined"
           >
-            <span>
-              <i class="fas fa-arrow-left"></i>
-              Back to All Matches
-            </span></a
+            {data.match.ourTeamName}
+          </a>
+        </h2>
+      </div>
+      <div class="column is-2">
+        <h1 class="title is-1 has-text-white">
+          {setsWon.our}
+          :
+          {setsWon.opp}
+        </h1>
+      </div>
+      <div class="column is-5">
+        <h2 class="has-text-left title has-text-white">
+          {data.match.oppTeamName}
+        </h2>
+      </div>
+    </div>
+    <div class="columns is-centered">
+      {#each data.match.points as { our, opp }}
+        <div class="column is-1 is-flex is-justify-content-center">
+          <h3
+            class="subtitle has-text-white"
+            class:has-text-weight-bold={our > opp}
           >
+            {our}
+          </h3>
+          <h3 class="subtitle has-text-white">-</h3>
+          <h3
+            class="subtitle has-text-white"
+            class:has-text-weight-bold={opp > our}
+          >
+            {opp}
+          </h3>
         </div>
-      </div>
-      <h2 class="title has-text-white">
-        {new Date(data.match.time).toDateString()}
-      </h2>
-      <h3 class="subtitle has-text-white">{data.match.location}</h3>
-      <div class="columns is-centered is-vcentered">
-        <div class="column is-5">
-          <h2 class="has-text-right title">
-            <a
-              href="/app/teams/{data.match.teamId}"
-              class="has-text-white is-underlined"
-            >
-              {data.match.ourTeamName}
-            </a>
-          </h2>
-        </div>
-        <div class="column is-2">
-          <h1 class="title is-1 has-text-white">
-            {setsWon.our}
-            :
-            {setsWon.opp}
-          </h1>
-        </div>
-        <div class="column is-5">
-          <h2 class="has-text-left title has-text-white">
-            {data.match.oppTeamName}
-          </h2>
-        </div>
-      </div>
-      <div class="columns is-centered">
-        {#each data.match.points as { our, opp }}
-          <div class="column is-1 is-flex is-justify-content-center">
-            <h3
-              class="subtitle has-text-white"
-              class:has-text-weight-bold={our > opp}
-            >
-              {our}
-            </h3>
-            <h3 class="subtitle has-text-white">-</h3>
-            <h3
-              class="subtitle has-text-white"
-              class:has-text-weight-bold={opp > our}
-            >
-              {opp}
-            </h3>
-          </div>
-        {/each}
-      </div>
+      {/each}
+    </div>
+  </div>
+</div>
+
+<!-- print-only header for print-friendly layout -->
+<div class="print-only">
+  <div class="section">
+    <div class="container">
+      <h1 class="title">
+        {data.match.ourTeamName} vs {data.match.oppTeamName}
+      </h1>
+      <table class="table">
+        <tr>
+          <th>{data.match.ourTeamName}</th>
+          {#each data.match.points as { our }}
+            <td>{our}</td>
+          {/each}
+          <td>{setsWon.our}</td>
+        </tr>
+        <tr>
+          <th>{data.match.oppTeamName}</th>
+          {#each data.match.points as { opp }}
+            <td>{opp}</td>
+          {/each}
+          <td>{setsWon.opp}</td>
+        </tr>
+      </table>
+      <p>
+        <strong>Time:</strong>
+        {new Date(data.match.time).toLocaleString()}
+      </p>
+      <p><strong>Location:</strong> {data.match.location}</p>
     </div>
   </div>
 </div>
@@ -129,3 +159,14 @@
     </div>
   </div>
 </div>
+
+<style>
+  .hero-body {
+    text-align: center;
+  }
+  @media print {
+    .hero-body {
+      text-align: unset !important;
+    }
+  }
+</style>
